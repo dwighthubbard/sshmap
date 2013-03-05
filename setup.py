0a612d0b848ff3e3226e3ce3a50e038243195060
Dwight Hubbard <dhubbard@yahoo-inc.com>
2013-03-05 01:39:31 +0000

    More fixes to the setup.py

--- setup.py
+++ setup.py
@@ -18,14 +18,13 @@ from distutils.core import setup
 
 setup(
   name = "sshmap",
-  version = "0.5.2",
+  version = "0.5.3",
   author = "Dwight Hubbard",
   author_email = "dhubbard@yahoo-inc.com",
   url = "https://github.com/dwighthubbard/sshmap",
   license = "LICENSE.txt",
   packages = ["sshmap"],
-  data_files = [('/usr/lib/hostlists/plugins',['hostlists_plugins/file.py','hostlists_plugins/dns.py','hostlists_plugins/dnsip.py','hostlists_plugins/range.py','hostlists_plugins/haproxy.py','hostlists_plugins/type.py'])],
-  scripts = ["sshmap/sshmap",'hostlists/hostlists',"hostlists/hostlists.py"],
+  scripts = ["sshmap/sshmap"],
   long_description = open('README.txt').read(),
   description = "A SSH Multiplexer designed to use ssh to perform map/reduce like operations",
   requires = ['ssh','hostlists','django'],
