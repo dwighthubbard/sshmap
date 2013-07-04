afa4266d3eac242a90b5cd27d5f75f2b325d6cd1
Dwight Hubbard <d@dhub.me>
2013-07-03 22:23:15 -0700

    Clean up the copyright message

--- sshmap/sshmap.py
+++ sshmap/sshmap.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
+#Copyright (c) 2012 Yahoo! Inc. All rights reserved.
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. See accompanying LICENSE file.
 """
  Python based ssh multiplexer optimized for map operations
-
- Copyright (c) 2012 Yahoo! Inc. All rights reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,   
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License. See accompanying LICENSE file.
 """
 #disable deprecated warning messages
 import warnings
@@ -667,7 +666,7 @@ def run(host_range, command, username=None, password=None, sudo=False, script=No
     status_clear()
     status_info(output_callback, 'Sending %d commands to each process' % chunksize)
     if callback_status_count in output_callback:
-        callback_status_count(ssh_result(parm = results.parm))
+        callback_status_count(ssh_result(parm=results.parm))
         
     try:
         for result in map_command(run_command,
