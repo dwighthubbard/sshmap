d4e20bafb6e764e82c7710476c22716261088e3b
dhubbard <dhubbard@yahoo-inc.com>
2013-07-08 18:38:43 -0700

    Split out callbacks again

--- sshmap/sshmap.py
+++ sshmap/sshmap.py
@@ -22,17 +22,12 @@ warnings.filterwarnings("ignore")
 # Python Standard Library imports
 import sys
 import os
-import stat
 import getpass
 import socket
 import types
-import base64
 import random
 import signal
-import hashlib
-import json
 import multiprocessing
-import subprocess
 import logging
 
 # Imports from external python extension modules
@@ -40,6 +35,7 @@ import ssh
 
 # Imports from other sshmap modules
 import hostlists
+import sshmap.callback
 
 # Defaults
 JOB_MAX = 100
@@ -427,7 +423,8 @@ def status_info(callbacks, text):
     """
     #print callbacks,text
     #return
-    if isinstance(callbacks, list) and callback_status_count in callbacks:
+    if isinstance(callbacks, list) and \
+            sshmap.callback.status_count in callbacks:
         status_clear()
         sys.stderr.write(text)
         sys.stderr.flush()
@@ -441,209 +438,6 @@ def status_clear():
     #sys.stderr.flush()
 
 
-# Built in callbacks
-# Filter callback handlers
-def callback_flowthrough(result):
-    """
-    Builtin Callback, return the raw data passed
-
-    >>> result=callback_flowthrough(ssh_result(["output"], ["error"],"foo", 0))
-    >>> result.dump()
-    foo output error 0 0 None
-    """
-    return result
-
-
-def callback_summarize_failures(result):
-    """
-    Builtin Callback, put a summary of failures into parm
-    """
-    failures = result.setting('failures')
-    if not failures:
-        result.parm['failures'] = []
-        failures = []
-    if result.ssh_retcode:
-        failures.append(result.host)
-        result.parm['failures'] = failures
-    return result
-
-
-def callback_exec_command(result):
-    """
-    Builtin Callback, pass the results to a command/script
-    :param result:
-    """
-    script = result.setting("callback_script")
-    if not script:
-        return result
-    status_clear()
-    result_out, result_err = subprocess.Popen(
-        script + " " + result.host,
-        shell=True,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    ).communicate(
-        result.out_string() + result.err_string()
-    )
-    result.out = [result_out]
-    result.err = [result_err]
-    print(result.out_string())
-    return result
-
-
-def callback_aggregate_output(result):
-    """ Builtin Callback, Aggregate identical results """
-    aggregate_hosts = result.setting('aggregate_hosts')
-    if not aggregate_hosts:
-        aggregate_hosts = {}
-    collapsed_output = result.setting('collapsed_output')
-    if not collapsed_output:
-        collapsed_output = {}
-    h = hashlib.md5()
-    h.update(result.out_string())
-    h.update(result.err_string())
-    if result.ssh_retcode:
-        h.update(result.ssh_error_message())
-    digest = h.hexdigest()
-    if digest in aggregate_hosts.keys():
-        aggregate_hosts[digest].append(result.host)
-    else:
-        aggregate_hosts[digest] = [result.host]
-        if result.ssh_retcode:
-            error = []
-            if result.err:
-                error = result.err
-            error.append(result.ssh_error_message())
-            collapsed_output[digest] = (result.out, error)
-        else:
-            collapsed_output[digest] = (result.out, result.err)
-    result.parm['aggregate_hosts'] = aggregate_hosts
-    if collapsed_output:
-        result.parm['collapsed_output'] = collapsed_output
-    return result
-
-
-def callback_filter_match(result):
-    """
-    Builtin Callback, remove all output if the string is not found in the
-    output
-    similar to grep
-    :param result:
-    """
-    if result.out_string().find(result.setting('match')) == -1 and \
-            result.err_string().find(result.setting('match')) == -1:
-        result.out = ''
-        result.err = ''
-    return result
-
-
-def callback_filter_json(result):
-    """
-    Builtin Callback, change stdout to json
-
-    >>> result=callback_filter_json(ssh_result(["output"], ["error"],"foo", 0))
-    >>> result.dump()
-    foo [["output"], ["error"], 0] error 0 0 None
-    """
-    result.out = [json.dumps((result.out, result.err, result.retcode))]
-    return result
-
-
-def callback_filter_base64(result):
-    """
-    Builtin Callback, base64 encode the info in out and err streams
-    """
-    result.out = [base64.b64encode(result.out_string)]
-    result.err = [base64.b64encode(result.err_string)]
-    return result
-
-
-#Status callback handlers
-def callback_status_count(result):
-    """
-    Builtin Callback, show the count complete/remaining
-    :param result:
-      """
-    # The master process inserts the status into the
-    # total_host_count and completed_host_count variables
-    sys.stderr.write('\x1b[0G\x1b[0K%s/%s' % (
-        result.setting('completed_host_count'),
-        result.setting('total_host_count')))
-    sys.stderr.flush()
-    return result
-
-
-#Output callback handlers
-def callback_output_prefix_host(result):
-    """
-    Builtin Callback, print the output with the hostname: prefixed to each line
-    :param result:
-
-    >>> result=callback_output_prefix_host(ssh_result(['out'],['err'], 'hostname', 0))
-    hostname: out
-    >>> result.dump()
-    hostname hostname: out hostname: Error: err 0 0 None
-    """
-    output = []
-    error = []
-    status_clear()
-    # If summarize_failures option is set don't print ssh errors inline
-    if result.setting('summarize_failed') and result.ssh_retcode:
-        return result
-    if result.setting('print_rc'):
-        rc = ' SSH_Returncode: %d\tCommand_Returncode: %d' % (
-            result.ssh_retcode, result.retcode)
-    else:
-        rc = ''
-    if result.ssh_retcode:
-        print >> sys.stderr, '%s: %s' % (result.host, result.ssh_error_message())
-        error = ['%s: %s' % (result.host, result.ssh_error_message())]
-    if len(result.out_string()):
-        for line in result.out:
-            if line:
-                print '%s:%s %s' % (result.host, rc, line.strip())
-                output.append('%s:%s %s\n' % (result.host, rc, line.strip()))
-    if len(result.err_string()):
-        for line in result.err:
-            if line:
-                print >> sys.stderr, '%s:%s %s' % (result.host, rc, line.strip())
-                error.append('%s:%s Error: %s\n' % (result.host, rc, line.strip()))
-    if result.setting('output'):
-        if not len(result.out_string()) and not len(result.err_string()) and not result.setting(
-                'only_output') and result.setting('print_rc'):
-            print '%s:%s' % (result.host, rc)
-        sys.stdout.flush()
-        sys.stderr.flush()
-    result.out = output
-    result.err = error
-    return result
-
-
-def read_conf(key=None, prompt=True):
-    """ Read settings from the config file """
-    try:
-        conf = json.load(open(os.path.expanduser('~/.sshmap.conf'), 'r'))
-    except IOError:
-        conf = conf_defaults
-    if key:
-        try:
-            return conf[key].encode('ascii')
-        except KeyError:
-            pass
-    else:
-        return conf
-    if key and prompt:
-        conf[key] = raw_input(conf_desc[key] + ': ')
-        fh = open(os.path.expanduser('~/.sshmap2.conf'), 'w')
-        os.fchmod(fh.fileno(), stat.S_IRUSR | stat.S_IWUSR)
-        json.dump(conf, fh)
-        fh.close()
-        return conf[key]
-    else:
-        return None
-
-
 def init_worker():
     """ Set up the signal handler for new worker threads """
     signal.signal(signal.SIGINT, signal.SIG_IGN)
@@ -651,8 +445,8 @@ def init_worker():
 
 def run(host_range, command, username=None, password=None, sudo=False,
         script=None, timeout=None, sort=False, bufsize=-1, cwd='/tmp',
-        jobs=None, output_callback=callback_summarize_failures, parms=None,
-        shuffle=False, chunksize=None):
+        jobs=None, output_callback=sshmap.callback.summarize_failures,
+        parms=None, shuffle=False, chunksize=None):
     """
     Run a command on a hostlists host_range of hosts
     :param host_range:
@@ -740,16 +534,16 @@ def run(host_range, command, username=None, password=None, sudo=False,
         map_command = pool.imap_unordered
 
     if isinstance(output_callback, types.ListType) and \
-            callback_status_count in output_callback:
-        callback_status_count(ssh_result(parm=results.parm))
+            sshmap.callback.status_count in output_callback:
+        sshmap.callback.status_count(ssh_result(parm=results.parm))
 
     # Create a process pool and pass the parameters to it
 
     status_clear()
     status_info(
         output_callback, 'Sending %d commands to each process' % chunksize)
-    if callback_status_count in output_callback:
-        callback_status_count(ssh_result(parm=results.parm))
+    if sshmap.callback.status_count in output_callback:
+        sshmap.callback.status_count(ssh_result(parm=results.parm))
         
     try:
         for result in map_command(
@@ -779,7 +573,7 @@ def run(host_range, command, username=None, password=None, sudo=False,
     #  pass
     pool.terminate()
     if isinstance(output_callback, types.ListType) and \
-            callback_status_count in output_callback:
+            sshmap.callback.status_count in output_callback:
         status_clear()
     return results
 
