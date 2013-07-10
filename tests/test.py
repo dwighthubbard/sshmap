932fc7c04fdab6eb0ad0dc0d7f14e205f77a9a54
dhubbard <dhubbard@yahoo-inc.com>
2013-07-10 15:34:29 -0700

    Add unittest

---
+++ tests/test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#Copyright (c) 2012 Yahoo! Inc. All rights reserved.
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. See accompanying LICENSE file.
+__author__ = 'dhubbard'
+import sshmap
+import os
+import unittest
+
+class TestSSH(unittest.TestCase):
+
+    def set_up(self):
+        pass
+
+    def test_shell_command_as_user(self):
+        # Run a ssh command to localhost and verify it works
+        result = os.popen('sshmap/sshmap localhost echo hello').read().strip()
+        self.assertEqual('localhost: hello', result)
+
+    #def test_shell_command_with_sudo(self):
+        # Run a ssh command to localhost and verify it works
+        #result = os.popen('sshmap/sshmap --sudo localhost echo hello').read().strip()
+        #self.assertEqual('localhost: hello', result)
+
+    def test_shell_script_as_user(self):
+        # Run a ssh command to localhost and verify it works
+        open('testscript.test').write('#!/bin/bash\necho hello\n')
+        result = os.popen('sshmap/sshmap localhost --runscript testscript.test').read().strip()
+        self.assertEqual('localhost: hello', result)
+        os.remove('testscript.test')
+
+    def test_shell_script_as_root(self):
+
+if __name__ == '__main__':
+    unittest.main()
