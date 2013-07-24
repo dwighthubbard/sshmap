7fe4ef63f7a0e457202beb90014546ec1296473f
dhubbard <dhubbard@yahoo-inc.com>
2013-07-24 10:41:34 -0700

    Update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(
     name="sshmap",
-    version="0.5.47",
+    version="0.5.48",
     author="Dwight Hubbard",
     author_email="dhubbard@yahoo-inc.com",
     url="https://github.com/dwighthubbard/sshmap",
