870893fdeead99a69f741ccd1a6baf9a44608a9b
dhubbard <dhubbard@yahoo-inc.com>
2013-07-26 16:23:28 -0700

    Dependency fixes for setup.py

--- setup.py
+++ setup.py
@@ -19,15 +19,15 @@ from distutils.core import setup
 
 setup(
     name="sshmap",
-    version="0.5.51",
+    version="0.5.56",
     author="Dwight Hubbard",
     author_email="dhubbard@yahoo-inc.com",
-    url="https://github.com/dwighthubbard/sshmap",
+    url="https://github.com/yahoo/sshmap",
     license="LICENSE.txt",
     packages=["sshmap"],
     scripts=["sshmap/sshmap"],
     long_description=open('README.txt').read(),
     description="A SSH Multiplexer designed to use ssh to perform map/reduce"
                 " operations",
-    requires=['ssh', 'hostlists', 'django'],
+    install_requires=['ssh', 'hostlists', 'django'],
 )
