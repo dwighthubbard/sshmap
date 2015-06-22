df86a624df34e886abb30f6074cf29d3fd8ba88f
Dwight Hubbard <dhubbard@yahoo-inc.com>
2015-06-22 11:25:14 -0700

    Simplify imports and remove the try/except

--- sshmap/__init__.py
+++ sshmap/__init__.py
@@ -11,34 +11,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License. See accompanying LICENSE file.
 
-try:
-    import sshmap.sshmap
-    import sshmap.callback
-    import sshmap.utility
-    import sshmap.runner
+from . import sshmap
+from . import callback
+from . import utility
+from . import runner
 
-    # For backwards compatibility
-    from sshmap.callback import summarize_failures as \
-        callback_summarize_failures
-    from sshmap.callback import aggregate_output as callback_aggregate_output
-    from sshmap.callback import exec_command as callback_exec_command
-    from sshmap.callback import filter_match as callback_filter_match
-    from sshmap.callback import status_count as callback_status_count
+from .callback import summarize_failures as callback_summarize_failures
+from .callback import aggregate_output as callback_aggregate_output
+from .callback import exec_command as callback_exec_command
+from .callback import filter_match as callback_filter_match
+from .callback import status_count as callback_status_count
 
-    # The actual used sshmap functions
-    from sshmap.sshmap import run, run_command, run_with_runner
-except ImportError:
-    import sshmap
-    import callback
-    import utility
-    import runner
-
-    # For backwards compatibility
-    from callback import summarize_failures as callback_summarize_failures
-    from callback import aggregate_output as callback_aggregate_output
-    from callback import exec_command as callback_exec_command
-    from callback import filter_match as callback_filter_match
-    from callback import status_count as callback_status_count
-
-    # The actual used sshmap functions
-    from sshmap import run, run_command, run_with_runner
+from .sshmap import run, run_command, run_with_runner
